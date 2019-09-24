bfdcb6e9b00f82790322643c1ae1f70ff06e097f
Hector Leo <kazama_leo19@hotmail.com>
2019-09-24 15:34:18 -0500

    cambio 40

--- database/seeds/TablaPermisoSeeder.php
+++ database/seeds/TablaPermisoSeeder.php
@@ -12,6 +12,6 @@ class TablaPermisoSeeder extends Seeder
      */
     public function run()
     {
-        factory(Permiso::class, 30)->create();
+        factory(Permiso::class, 40)->create();
     }
 }
